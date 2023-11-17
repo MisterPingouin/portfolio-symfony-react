1f13eebbee5122dd84f8fd2aca1be176f0790b12
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-11-17 11:32:45 +0100

    Base 1

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./assets/**/*.js', './templates/**/*.html.twig'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
