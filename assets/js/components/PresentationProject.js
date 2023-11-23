10e1a0c1d83c97d9bb4666bb6b355b5f031c3119
MisterPingouin <anthony.axeldisplay@gmail.com>
2023-11-23 18:16:37 +0100

    debug done / portfolio done in react + symfony / need Crud and improve

--- assets/js/components/PresentationProject.js
+++ assets/js/components/PresentationProject.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import HeaderPres from './HeaderPres';
+import ScrollDownIndicator from './ScrollDownIndicator';
 import '../../styles/presentation.scss';
 
 const PresentationProject = () => {
@@ -64,33 +65,35 @@ const PresentationProject = () => {
       <section className="font-titlefont h-full">
         {presentations.map((presentation, index) => (
           <article id={`article-${index}`} key={index} className={index === 0 ? 'current static' : ''}>
-            <div className="image-wrap">
-              <img 
-                className="rounded shadow-sm" 
-                src={presentation.link} 
-                alt={presentation.fonction}
-                onError={() => setImageError(true)}
-              />
+          <div className="image-wrap">
+            <img 
+              className="rounded shadow-sm" 
+              src={presentation.link} 
+              alt={presentation.fonction}
+              onError={() => setImageError(true)}
+            />
+          </div>
+          <div className="mresponsive w-full md:h-full p-3 flex flex-col md:justify-center items-start relative">
+            <div className="flex flex-col justify-center items-start md:h-full md:max-w-[50%] content-wrapper md:pl-[3rem] md:pt-6">
+              <h1 className="md:pl-3 md:pb-2 mb-2 font-bold md:text-left w-full text-2xl md:text-6xl text-accentcolor">{presentation.fonction}</h1>
+              <p className="md:pl-3 mt-4 pt-4 w-full font-contentfont font-semibold text-gray-700 text-left">{presentation.description}</p>
+              {index === presentations.length - 1 && presentation.website && (
+                <div className="unique-btn-container self-center mt-3 md:mt-[5em] font-contentfont font-medium text-accentcolor text-sm md:text-md">
+                  <a href={presentation.website} target="_blank" rel="noopener noreferrer">
+                    <button className="learn-more cta-btn">
+                      <div className="circle">
+                        <span className="icon arrow"></span>
+                      </div>
+                      <span className="button-text">Go to the website</span>
+                    </button>
+                  </a>
+                </div>
+              )}
+                          {index === 0 && <ScrollDownIndicator />}
+
             </div>
-            <div className="mresponsive w-full md:h-full p-3 flex flex-col md:justify-center items-start relative">
-              <div className="flex flex-col justify-center items-start md:h-full md:max-w-[50%] content-wrapper md:pl-[3rem] md:pt-6">
-                <h1 className="md:pl-3 md:pb-2 mb-2 font-bold md:text-left w-full text-2xl md:text-6xl text-accentcolor">{presentation.fonction}</h1>
-                <p className="md:pl-3 mt-4 pt-4 w-full font-contentfont font-semibold text-gray-700 text-left">{presentation.description}</p>
-                {presentation.website && (
-                  <div className="unique-btn-container self-center mt-3 md:mt-[5em] font-contentfont font-medium text-accentcolor text-sm md:text-md">
-                    <a href={presentation.website} target="_blank" rel="noopener noreferrer">
-                      <button className="learn-more cta-btn">
-                        <div className="circle">
-                          <span className="icon arrow"></span>
-                        </div>
-                        <span className="button-text">Go to the website</span>
-                      </button>
-                    </a>
-                  </div>
-                )}
-              </div>
-            </div>
-          </article>
+          </div>
+        </article>
         ))}
       </section>
     </>
---
+++ assets/js/components/ScrollDownIndicator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ScrollDownIndicator = () => (
+    <div className="scrolldown">
+      <div className="chevrons">
+        <div className='chevrondown'></div>
+        <div className='chevrondown'></div>
+      </div>
+    </div>
+  );
+
+export default ScrollDownIndicator ;
